9c942e262ed689afb9ef789e5233388c7d09afd2
Marius <marius@example.com>
2022-07-08 15:27:30 +0300

    uzd

--- js/dom.js
+++ js/dom.js
@@ -12,7 +12,17 @@ const rez = textEl.textContent;
 console.log('rez ===', rez);
 
 // prideti nauja elementa i div su id box
+// memoryLocation 00xxlsq
 const boxEl = document.getElementById('box');
 // innerHTML - sugeneruoti html struktura is string
 boxEl.innerHTML = '<h3>As esu dinamiskas <i>elementas</i></h3>';
 // boxEl.textContent = '<h3>As esu dinamiskas <i>elementas</i></h3>';
+
+/*
+ <article>
+    <h2>Main title</h2> pakeisti i "pagrindinte antraste"
+    <p>Date</p> pakeisti i 'Penktadienis'
+    <p>Lorem ipsum dolor </p> darasyti prie esamo teksto " == sugeneruotas textas"
+  </article>
+  <li>Yellow</li> pakeisti paskutine spalva i 'violet'
+*/
